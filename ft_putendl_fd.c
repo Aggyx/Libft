2a07b815675e53469d124e1a4f7b09e427391ec6
Santiago Magniny <smagniny@c1r15s6.42madrid.com>
2023-06-17 14:00:01 +0200

    fix putendl_fd

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smagniny <smagniny@student.42madrid.com    +#+  +:+       +#+        */
+/*   By: smagniny <smagniny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 18:31:16 by smagniny          #+#    #+#             */
-/*   Updated: 2022/10/10 18:40:41 by smagniny         ###   ########.fr       */
+/*   Updated: 2023/06/17 13:59:33 by smagniny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,4 @@ void	ft_putendl_fd(char *s, int fd)
 	if (!s || !fd)
 		return ;
 	ft_putstr_fd(s, fd);
-	write(fd, "\n", 1);
 }
